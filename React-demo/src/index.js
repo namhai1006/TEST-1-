ec2fc9b388594b2d8f910fd1e7b3a1d4a67a899a
hai <namhai1006@gmail.com>
2024-01-10 14:15:24 +0700

    Add Boostrap & Jquery

--- React-demo/src/index.js
+++ React-demo/src/index.js
@@ -1,6 +1,6 @@
 console.log("Đây là index.js");
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/EX4/EX4-3";
+import App from "./components/LAB1/EX4-JSXvsES6-4/Shape";
 
 ReactDOM.render(<App />, document.getElementById("root"));
